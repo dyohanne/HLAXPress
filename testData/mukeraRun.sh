5ed30cf899814f4526859c975aaa47e528b3a4ac
dyohanne <dawit@puhti-login1.bullx>
2020-09-04 13:10:50 +0300

    change addresses

--- testData/mukeraRun.sh
+++
@@ -1,6 +0,0 @@
-# for illumina read data
-Rscript HLAXPress.R testData testData/testDataHLAs --hlaRef totalHLAdb/hladb --readType illumina
-
-# for nanopore data (three separate fastq.gz files in testData ), this is not yet tested, it had some error, check it
-# hla allele info of sample47 from illumina used for BC01, for quick testing
-Rscript HLAXPress.R testData testData/testDataHLAs --samName BC01 --hlaRef totalHLAdb/hladb --readType nanopore
