c652a615afe43a3719f56b0d2def7fb0917e56a5
dyohanne <dawit@puhti-login1.bullx>
2020-09-04 14:35:18 +0300

    modify the readme

--- testData/testRun.R
+++ testData/testRun.R
@@ -1,11 +1,7 @@
 
 source("../HLAXPress_fxns.R")
-ptm <- proc.time()
+getAlleleFreqCounts(sampleHlaData="testDataHLAs",sampleId="sample45",IlluminaDir="testData",nanDir=NULL)
 
 
-getAlleleFreqCounts(sampleHlaData="testDataHLAs",
-                    sampleId="sample45",IlluminaDir="testData",nanDir=NULL)
-
-print(proc.time() - ptm)
 
 
